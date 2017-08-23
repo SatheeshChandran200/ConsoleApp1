16d9a04dfb6cf1d4cca4e8d37e9b3f3d095cd929
satheesh chandran <satheeshchandran200@hotmail.com>
2017-08-23 21:08:42 +0530

    Changes to include new class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApp1
             Console.WriteLine(1 + 2 + 3);
             Console.WriteLine("Hello World!");
             Console.WriteLine("This is a new line");
+            Test1 test = new Test1();
+            test.Count = 1;
+            Console.WriteLine(test.Count);
             Console.ReadLine();
         }
     }
