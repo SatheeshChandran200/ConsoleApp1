43a04457b7d19377389d4d87a7aabeb672f955d2
satheesh chandran <satheeshchandran200@hotmail.com>
2017-08-23 20:54:20 +0530

    Adding multiple files

---
+++ ConsoleApp1/Test1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Test1
+    {
+        private int _count;
+
+        public int Count
+        {
+            get { return _count; }
+            set { _count = value; }
+        }
+
+        
+    }
+}
