8be1c295fcbbf509443206061a5d75a9155a149b
satheesh chandran <satheesh chandran>
2017-08-23 20:04:56 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//sing System.Object;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
